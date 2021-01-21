7c4987dba94d865f7983c581cd93485710f6a02d
Francisco Orchard <f.orchard@epiconcept.fr>
2021-01-21 01:46:53 +0100

    updating to version 0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='kskit',
-    version='0.0.3',
+    version='0.0.4',
     description='Cancer screening analysis module build on Python',
     long_description=readme,
     long_description_content_type='text/markdown',
