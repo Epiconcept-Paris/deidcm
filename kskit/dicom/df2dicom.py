70988a49af904d66d7f36c9a6463e8427ba8bb46
Mickey-Miky-Low <william.madie0@gmail.com>
2021-10-14 17:52:06 +0200

    adding df2dicom simple attr + seq (not recursive)

--- kskit/dicom/df2dicom.py
+++ kskit/dicom/df2dicom.py
@@ -37,6 +37,7 @@ def df2dicom(dataframe, outdir):
   build_dicoms(dico)
 
 
+
 def build_dicoms(dico):
   """
   Gets all the attributes names and values and call for the build of each
@@ -61,6 +62,7 @@ def build_dicoms(dico):
   print(ds)
 
 
+
 def build_dicom(no_dicom, attr_names, attr_values):
   """
   Builds a single DICOM's dataset and returns it.
@@ -68,88 +70,136 @@ def build_dicom(no_dicom, attr_names, attr_values):
   @param attr_names : list of dictionaries containing element info
   @param attr_values : list of lists containing values for each element of all the DICOMs
   """
+  debug_list = []
   ds = pydicom.Dataset()
-
-  for i in range(len(attr_names)):
+  nb_sequences = 0
+  i = 0
+  while i < len(attr_names):
+    print("i réel : ", i)
     name = attr_names[i]
-    print(name[1], name[2], attr_values[i][no_dicom])
+
+    #debug
+    if name[1] in debug_list:
+      print(name[1], name[2], name[0], attr_values[i][no_dicom])
+      print("Déjà ajouté !")
+      exit(1)
+
+    print(name[1], name[2], name[0], attr_values[i][no_dicom])
     
     
     if (name[2] != 'SQ'):
+      value = encode_unit(attr_values[i][no_dicom], name[2])
       try:
-        ds.add_new(name[1], name[2], attr_values[i][no_dicom])
+        ds.add_new(name[1], name[2], value)
       except ValueError:
         #If the value is a multival (= list of values), it needs to be removed
         #from the string and properly cast as a multivalue compatible type
-        ds.add_new(name[1], name[2], ast.literal_eval(attr_values[i][no_dicom]))
-        
-     
-      
+        ds.add_new(name[1], name[2], ast.literal_eval(value))
+      debug_list.append(name[1])
+      i += 1
+
     else:
-      seq = build_sequence(no_dicom, attr_names, attr_values, i)
-      beam = pydicom.Dataset()
-      beam.BlockName = name[2]
-      ds.BeamSequence = Sequence([beam])
-      ds.BeamSequence[0].BlockSequence = Sequence(seq)
-      ds.BeamSequence[0].NumberOfBlocks = len(seq)
+      nb_sequences += 1
+      print(attr_values[i])
+      (seq, l) = build_sequence(no_dicom, attr_names, attr_values, i)
+      print(i, no_dicom)
       
+      ds.add_new(name[1], name[2], Sequence(seq))
+      ds.add_new("Columns", "US", 128)
+      #with open("/home/williammadie/images/deid/df2dicom_test/ds.txt", "a") as f:
+      #  f.write(f"\n\n\n\n{ds}\n\n\n\n")
+      debug_list.append(name[1])
+      i += (l-2)
+      print("attr suivant : ", attr_names[i][1], attr_names[i][2], attr_names[i][0], attr_values[i][no_dicom])
+      print("i suivant : ", i)
+    
       """
       TODO : CONTINUE TO BUILD SEQUENCE
       - find how to name the sequence with the correct element (tag and name)
       - check why build_sequence() didn't build the full sequence in the test (it built only one Data Element)
       - try to make build_sequence() for one layer (= one level)
       """
-    i += 1
+   
+  print(f"NB_SEQUENCE ====> {nb_sequences}")
   return ds
 
 
+
 def build_sequence(no_dicom, attr_names, attr_values, i, nb_previous_parents = 0):
+  #The sequence is a list of datasets
   sequence = []
-  ds = pydicom.Dataset()
   name = attr_names[i]
-  print(name)
+  """
+  TODO: remove debug
+  for i in range(i, i+30):
+    print(attr_names[i])
+  """
+  
+  #Determines the number of levels for recursive search of data elements
   nb_levels = -1
+  #Levels is a list which contains tuples like (dic_key_tag_attr, tag_attr)
+  #dic_key_tag_attr is the key associated to the tag_attr in the dic attr_names
   levels = []
+  #Finds all the tag_attr in the name. If there are n tag_attr, then there are
+  #n-1 levels
   for key, value in name.items():
     if '0x' in value:
       nb_levels += 1
       levels.append((key, value))
+    if '@' in value:
+      ind_in_sequence = int(value[1])
+  #print("IND_IN_SEQ = ", ind_in_sequence)   
   print(nb_levels)
-  print("Levels :", levels)
-  
+  #print("Levels :", levels)
+  if nb_levels > 2:
+    print("récursion requise")
+    exit(1)
   nb_treated_levels = 1
-  level = levels[len(levels)-1-nb_treated_levels][1]
+  parent_tag = levels[len(levels)-1-nb_treated_levels][1]
   ind_level = levels[len(levels)-1-nb_treated_levels][0]
 
+  #Browse through the list of attr_names of all dicom from the current attr
+  #until it meets a new sequence or the current sequence ends
+  ds = pydicom.Dataset()
+  l = 1
   for i in range(i, len(attr_names)):
+    name = attr_names[i]
+    #print(f"Tour n°{l}\n")
+    #print(attr_names[i])
+    l += 1
+    print(f"CHECK {attr_names[i][1]} <=> {parent_tag}")
+    if attr_names[i][1] != parent_tag:
+      print("Sequence suivante !")
+      break
+      
+    for attribute in attr_names[i].values():
+      if '@' in attribute:
+        #print(attribute)
+        current_ind_in_sequence = int(attribute[1])
+    
+    if current_ind_in_sequence != ind_in_sequence:
+      #print(f"Fin de dataset {current_ind_in_sequence} != {ind_in_sequence}")
+      #print("DS :\n", ds)
+      sequence.append(ds)
+      ds = pydicom.Dataset()
+      ind_in_sequence += 1
+
     if nb_treated_levels == len(levels):
-      return ds
+      break
     else:
+      #print(i, no_dicom)
+      print(name)
+      print(name[ind_level+6], name[ind_level+6], attr_values[i][no_dicom])
       ds.add_new(name[ind_level+6], name[ind_level+6], attr_values[i][no_dicom])
+
   sequence.append(ds)
-  return sequence
+  return (sequence, l)
 
 
-def encode_unit(value):
-  t = type(value)
-  if t == int:
-    return str(value)
-  elif t == str: 
-    return value
-  elif t == bytes: 
-    return base64.b64encode(value).decode("UTF-8")
-  elif t == pydicom.uid.UID:
-    return str(value)
-  elif t == pydicom.valuerep.DSfloat:
-    return str(value)
-  elif t == pydicom.valuerep.IS:
-    return str(value)
-  elif t == pydicom.valuerep.DT:
-    return str(value.timestamp())
-  elif t ==  pydicom.valuerep.PersonName:
-    return str(value.timestamp())
-  elif t == list and len(value)==0:
-    return '[]'
+
+def encode_unit(value, VR):
+  if VR == 'OB':
+    return base64.b64decode(value.encode("UTF-8"))
   else:
-    raise ValueError("cannot encode {t} as unit")
+    return value;
 
