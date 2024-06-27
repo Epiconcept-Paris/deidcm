18c7c4b95958d69836baa1b1a05984f77de2d02f
William Madié <61837630+williammadie@users.noreply.github.com>
2024-06-27 11:32:28 +0200

    Update setup.py
    add required version of Python

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open('README.md', encoding="utf-8") as f:
 setup(
     name='deidcm',
     version='0.0.3',
+    python_requires='>=3.6',
     description='Reusable toolset for deidentifiying images and metadata contained inside DICOM files',
     long_description=readme,
     long_description_content_type='text/markdown',
